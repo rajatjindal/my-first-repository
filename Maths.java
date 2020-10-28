4d799dd607cce56bd9a1d5eae4bea2b72e183da9
chhavi0326 <73017893+chhavi0326@users.noreply.github.com>
2020-10-28 15:53:36 +0530

    Update and rename Add.java to Maths.java

---
+++ Maths.java
@@ -0,0 +1,11 @@
+class Maths {
+     public static void main(String args[]) {
+        System.out.println("Hello, World");
+        int result = addtwonumbers(10, 20);
+        System.out.println(result);
+    }
+
+    public static int addtwonumbers(int a, int b) {
+        return a + b;
+    }
+}
