8d01cc037b048b22aaf27a84464ab7080d0dab66
Bharatbushan2526 <73333511+Bharatbushan2526@users.noreply.github.com>
2020-10-29 13:50:27 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,12 @@
 class Calculator {
 
-	public static void main(String[] args){
-	System.out.println("hello world");
-       int result = addTwoNumbers(10,20);
-       System.out.println(result);
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		int result = addTwoNumbers(10, 20);
+		System.out.println(result);
 	}
-	
-	public static int addTwoNumbers(int a, int b){
+
+	public static int addTwoNumbers(int a, int b) {
 		return a + b;
 	}
 }
