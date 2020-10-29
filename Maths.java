f3f8e0313098dbb6420a4afdb2b3faa4bbb560bf
chhavi0326 <73017893+chhavi0326@users.noreply.github.com>
2020-10-29 11:38:23 +0530

    Update Maths.java

--- Maths.java
+++ Maths.java
@@ -1,26 +1,26 @@
+
 class Maths {
     public static void main(String args[]) {
         System.out.println("Hello, World");
-        int result = addtwonumbers(10, 20);
+        int result = addTwoNumbers(10, 20);
         System.out.println(result);
     }
 
-    public static int addtwonumbers(int a, int b) {
+    public static int addTwoNumbers(int a, int b) {
         return a + b;
 
     }
 
-    public static int subtracttwonumbers(int a, int b) {
+    public static int subtractTwoNumbers(int a, int b) {
         return a - b;
     }
 
-    public static int productoftwonumbers(int a, int b) {
+    public static int productOfTwoNumbers(int a, int b) {
         return a * b;
     }
 
-    public static int divisionoftwonumbers(int a, int b) {
+    public static int divisionOfTwoNumbers(int a, int b) {
         return a / b;
     }
 
 }
-
