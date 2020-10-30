7947c280fc79771612ded24da20c95fd4285763f
Bharatbushan2526 <73333511+Bharatbushan2526@users.noreply.github.com>
2020-10-31 01:20:06 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,7 @@ class Calculator {
 		System.out.println("hello world");
 		int result = addTwoNumbers(10, 20);
 		int result1 = subTwoNumbers(20, 10);
-		int result2 = multiplyTwoNumbers(2, 8);
-		int result3 = divideTwoNumbers(8, 2);
-		System.out.println(result + "\n" + result1 + "\n" + result2 + "\n" + result3);
+		System.out.println(result + "\n" + result1);
 	}
 
 	public static int addTwoNumbers(int a, int b) {
