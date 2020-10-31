04e20581c2e61deb19300fef9d0d61c84640c91f
Amal Santhosh <42336393+amal-san@users.noreply.github.com>
2020-10-31 08:56:44 +0530

    add product of two numbers

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,7 @@ class Calculator {
 		System.out.println("hello world");
 		int result = addTwoNumbers(10, 20);
 		int result1 = subTwoNumbers(20, 10);
+		int product = productTwoNumbers(10,30);
 		System.out.println(result + "\n" + result1);
 	}
 
@@ -13,5 +14,8 @@ class Calculator {
 
 	public static int subTwoNumbers(int a, int b) {
 		return a - b;
+	}
+	public static int productTwoNumbers(int a, int b) {
+		return a * b;
 	}
 }
