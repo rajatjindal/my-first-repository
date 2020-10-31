4f9237b06c399172f547b23ad9e307cd2c2c5bd3
Amal Santhosh <42336393+amal-san@users.noreply.github.com>
2020-10-31 08:58:05 +0530

    System.out add to product

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@ class Calculator {
 		int result = addTwoNumbers(10, 20);
 		int result1 = subTwoNumbers(20, 10);
 		int product = productTwoNumbers(10,30);
-		System.out.println(result + "\n" + result1);
+		System.out.println(result + "\n" + result1 + "\n" + product);
 	}
 
 	public static int addTwoNumbers(int a, int b) {
